588dbbd97b40436b1af3b0e2e097827b7a0aa2dd
Mason Williams <masonmaxwellwilliams@gmail.com>
2023-10-08 19:58:09 -0400

    R13-FIO02-J Fixed issue by checking the return value of File.delete() and handling any possible errors

--- R13_FIO02_J.java
+++ R13_FIO02_J.java
@@ -2,5 +2,7 @@
 // FIO02 - Detect and handle file-related errors
 // https://wiki.sei.cmu.edu/confluence/display/java/FIO02-J.+Detect+and+handle+file-related+errors
 
-File file = new File(args[0]);
-file.delete();
+File file = new File("file");
+if (!file.delete()) {
+  // Deletion failed, handle error
+}
