fe291c331b53bff2642a8a3db9daac51b467fdc9
Mason Williams <masonmaxwellwilliams@gmail.com>
2023-10-08 19:37:13 -0400

    R03-NUM03-J Fixed issue and allowed integer to represent only the range of int datatype

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -2,5 +2,5 @@
 // NUM03 - Use integer types that can fully represent the possible range of unsigned data
 
 public static int getInteger(DataInputStream is) throws IOException {
-  return is.readInt(); 
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
 }
