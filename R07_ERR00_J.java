b80af03fff49454c83cc4840142053e92d34fe15
Mason Williams <masonmaxwellwilliams@gmail.com>
2023-10-08 19:47:23 -0400

    R07-ERR00-J Fixed ignored exceptions that could have been thrown. exceptions are not handled appropriately.

--- R07_ERR00_J.java
+++ R07_ERR00_J.java
@@ -2,8 +2,15 @@
 // ERR00 - Do not suppress or ignore checked exceptions
 // https://wiki.sei.cmu.edu/confluence/display/java/ERR00-J.+Do+not+suppress+or+ignore+checked+exceptions
 
-try {
-  //...
-} catch (IOException ioe) {
-  ioe.printStackTrace();
-}
+boolean validFlag = false;
+do {
+  try {
+    // ...
+    // If requested file does not exist, throws FileNotFoundException
+    // If requested file exists, sets validFlag to true
+    validFlag = true;
+  } catch (FileNotFoundException e) {
+    // Ask the user for a different file name
+  }
+} while (validFlag != true);
+// Use the file
