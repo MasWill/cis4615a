843b462e83be67e8199d99f8a9456254b1d584b2
Mason Williams <masonmaxwellwilliams@gmail.com>
2023-10-08 19:50:56 -0400

    R08-VNA00-J Fixed done flag by making it a volatile variable

--- R08_VNA00_J.java
+++ R08_VNA00_J.java
@@ -3,7 +3,7 @@
 // https://wiki.sei.cmu.edu/confluence/display/java/VNA00-J.+Ensure+visibility+when+accessing+shared+primitive+variables
 
 final class ControlledStop implements Runnable {
-  private boolean done = false;
+  private volatile boolean done = false;
   
   @Override public void run() {
     while (!done) {
